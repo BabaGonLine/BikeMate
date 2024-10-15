bd4e463ca03203d41509d08c3de899a7ef48126c
Gilad Erlich <babagonline@gmail.com>
2024-10-16 01:04:55 +0300

    WIP - error in auth when no token

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -17,7 +17,7 @@ export default function Auth() {
   const [isSigning, setIsSigningn] = useState(false);
   // const [isNewMail, setIsNewMail] = useState(true);
 
-  isTokenExpired();
+  // isTokenExpired();
 
   const isPosting = navigation.state === "submitting";
   function handleEmailChange() {
